fd21ac3ce5c0c2dfe01fdc21b0076f8a88819b8c
Anna Kompan <annakompanch@gmail.com>
2026-04-01 02:46:54 +0800

    w/o services

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LeftSideMenu } from './components/left-side-menu/left-side-menu';
+import { UserPanel } from './components/user-panel/user-panel';
+import { TopMenu } from './components/top-menu/top-menu';
+import { Preparation } from './components/preparation/preparation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    // Category,
+    // DeleteConfirmationModal,
+    // GenerateAnswerModal,
+    LeftSideMenu,
+    Preparation,
+    TopMenu,
+    // PageNotFound,
+    UserPanel,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
